a2bb1c3a59f9f37140b3cc8a146a4c739f8e11f8
joywek <joywek@qq.com>
2024-10-30 15:58:50 +0800

    Initial Commit

---
+++ libev/libev.h
@@ -0,0 +1,18 @@
+//
+//  libev.h
+//  libev
+//
+//  Created by Dylan on 2024/10/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libev.
+FOUNDATION_EXPORT double libevVersionNumber;
+
+//! Project version string for libev.
+FOUNDATION_EXPORT const unsigned char libevVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libev/PublicHeader.h>
+
+
